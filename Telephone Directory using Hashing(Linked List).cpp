943bb6aabff23b1b2d448b33817631a1ec11b863
Shubhu2603 <59830775+Shubhu2603@users.noreply.github.com>
2020-03-24 12:30:42 +0530

    Update and rename Assignment 7.cpp to Telephone Directory using Hashing(Linked List).cpp

--- Telephone Directory using Hashing(Linked List).cpp
+++ Telephone Directory using Hashing(Linked List).cpp
@@ -1,3 +1,5 @@
+/*Consider telephone book database of N clients. Make use of hash table implementation to quickly look up client's telephone number.*/
+
 #include <iostream>
 #include<string.h>
 using namespace std;
